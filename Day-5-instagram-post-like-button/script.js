dbabca734c2e8dfb005b54642d192c92ad7de6a5
Sameer <sk6586418@gmail.com>
2025-11-20 17:29:49 +0530

    add DOM day 5 task

--- Day-5-instagram-post-like-button/script.js
+++ Day-5-instagram-post-like-button/script.js
@@ -5,10 +5,8 @@ let post = document.querySelector('.post');
 let love = document.querySelector('#love');
 
 
-
-like.addEventListener("dblclick", () => {
-    like.classList.remove('ri-heart-line');
-    like.classList.add('ri-heart-fill');
+like.addEventListener("click", () => {
+    like.classList.replace('ri-heart-line', 'ri-heart-fill');
     like.style.color = 'red';
 
     h3.innerHTML = `69 likes`
@@ -29,30 +27,24 @@ like.addEventListener("dblclick", () => {
     }, 1400)
 });
 
-like.addEventListener('click', () => {
-    like.classList.remove('ri-heart-fill');
-    like.classList.add('ri-heart-line');
+like.addEventListener('dblclick', () => {
+    like.classList.replace('ri-heart-fill', 'ri-heart-line');
     like.style.color = 'black';
 
     h3.innerHTML = `68 likes`
 })
 
 bkm.addEventListener('click', () => {
-    bkm.classList.remove('ri-bookmark-line');
-    bkm.classList.add('ri-bookmark-fill');
-
+    bkm.classList.replace('ri-bookmark-line', 'ri-bookmark-fill');
 });
 
 bkm.addEventListener('dblclick', () => {
-    bkm.classList.remove('ri-bookmark-fill');
-    bkm.classList.add('ri-bookmark-line');
+    bkm.classList.remove('ri-bookmark-fill', 'ri-bookmark-line');
 });
-
 
 post.addEventListener('dblclick', () => {
 
-    like.classList.remove('ri-heart-line');
-    like.classList.add('ri-heart-fill');
+    like.classList.replace('ri-heart-line', 'ri-heart-fill');
     like.style.color = 'red';
 
     h3.innerHTML = `69 likes`;
